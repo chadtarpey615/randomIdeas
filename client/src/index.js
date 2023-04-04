66e57b6d02d54e8ae8b681a23a7ca7612acd0b75
Chad Tarpey <chadtarpey@gmail.com>
2023-04-04 17:40:42 -0500

    created ideaList class

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
 import Modal from './components/Modal';
 import IdeaForm from './components/IdeaForm'
+import IdeaList from './components/IdeaList'
 import './css/style.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 const modal = new Modal();
+
 const ideaForm = new IdeaForm();
 ideaForm.render();
+
+const ideaList = new IdeaList();
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const express = require("express")
 require("dotenv").config()
+const cors = require("cors")
 const port = process.env.PORT || 5000
 const connectDb = require("./config/db")
 const app = express()
@@ -15,6 +16,13 @@ app.use(express.static(path.join(__dirname, "public")))
 //body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+
+//cors middleware
+app.use(cors({
+    origin: ["http://localhost:5000", "http://localhost:3000"],
+    credentials: true
+}))
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to the RandomIdeas API" })
